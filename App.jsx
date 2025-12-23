850cb02fe2c271f545c8026f06f2e167dfa0433b
janshichauhan <jchauhan713@rku.ac.in>
2025-12-23 21:10:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import GreetingClass from "./C22122025/first_class" ;
+
+    
+  
+function App() {
+
+
+  return (
+    <>
+ 
+      <h1>Hello, World!</h1>
+      <br />
+      <GreetingClass />
+    </>
+  );
+}
+
+export default App;
---
+++ first_class.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+class GreetingClass extends Component {
+    render() {
+        return (
+        <>
+ <div>
+    <h1>This is First Class Component</h1>
+    <p>This is created using Class based component</p>
+ </div>
+ </>
+    );
+    }
+
+}
+export default GreetingClass;
